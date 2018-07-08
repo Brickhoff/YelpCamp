b0c06f49520dee9d2fe7d9c521f70683cab7a3d2
Brickhoff <9031201@qq.com>
2018-07-08 05:44:04 +0000

    Add campground model

---
+++ models/campground.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   imageId: String,
+   description: String,
+   price: Number,
+   location: String,
+   lat: Number,
+   lng: Number,
+   createdAt: {
+      type: Date,
+      default: Date.now
+   },
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
